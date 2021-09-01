32be73555302272bbd688063209224cdef7e3409
xinchen2019 <1312837959@qq.com>
2021-09-01 18:14:22 +0800

    提交flume poll

--- src/main/java/com/apple/core/ActionOperation.java
+++ src/main/java/com/apple/core/ActionOperation.java
@@ -28,13 +28,16 @@ public class ActionOperation {
         JavaSparkContext sc = new JavaSparkContext(conf);
         List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         JavaRDD<Integer> numbers = sc.parallelize(numberList);
-        JavaRDD<Integer> doubleNumbers = numbers.map(new Function<Integer, Integer>() {
+        JavaRDD<Integer> doubleNumbers = numbers.map(
+                new Function<Integer, Integer>() {
+                    private static final long serialVersionUID = 1L;
 
-            @Override
-            public Integer call(Integer v1) throws Exception {
-                return v1 * 2;
-            }
-        });
+
+                    @Override
+                    public Integer call(Integer v1) throws Exception {
+                        return v1 * 2;
+                    }
+                });
         List<Integer> doubleNumberList = doubleNumbers.collect();
         for (Integer num : doubleNumberList) {
             System.out.println(num);
