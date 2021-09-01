ae3466af93c4daa1c037b0178684b63c9f4bd1c7
xinchen2019 <1312837959@qq.com>
2021-09-01 22:53:55 +0800

    格式化代码

---
+++ src/main/java/com/apple/streaming/JavaCustomReceiver.java
@@ -0,0 +1,12 @@
+package com.apple.streaming;
+
+/**
+ * @Program: spark-java
+ * @ClassName: JavaCustomReceiver
+ * @Description: TODO
+ * @Author Mr.Apple
+ * @Create: 2021-09-01 22:42
+ * @Version 1.1.0
+ **/
+public class JavaCustomReceiver {
+}
